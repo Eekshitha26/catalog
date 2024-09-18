5573f092514f0f25eabe5055c4876bca1696fe3e
Eekshitha <150688770+Eekshitha26@users.noreply.github.com>
2024-09-18 13:16:50 +0530

    Keys.java

---
+++ Keys.java
@@ -0,0 +1,13 @@
+package com.shamirsecretsharing;
+
+public class Keys {
+	private int[] shares;
+
+    public Keys(int[] shares) {
+        this.shares = shares;
+    }
+
+    public int[] getShares() {
+        return shares;
+    }
+}
