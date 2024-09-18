9c033131f54801e32a1135d9529b0de3ac05416f
Eekshitha <150688770+Eekshitha26@users.noreply.github.com>
2024-09-18 13:17:37 +0530

    Root.java

---
+++ Root.java
@@ -0,0 +1,25 @@
+package com.shamirsecretsharing;
+
+/*public class Root {
+	 private int[] coefficients;
+
+	    public Root(int[] coefficients) {
+	        this.coefficients = coefficients;
+	    }
+
+	    public int[] getCoefficients() {
+	        return coefficients;
+	    }
+}
+*/
+public class Root {
+    private int[] coefficients;
+
+    public Root(int[] coefficients) {
+        this.coefficients = coefficients;
+    }
+
+    public int[] getCoefficients() {
+        return coefficients;
+    }
+}
